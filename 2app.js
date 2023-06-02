97df5104a42533da1beafbfd1780e4378600951a
Shanaz King <sking1128@YewYew1128.localdomain>
2023-06-02 18:33:54 -0400

    adding changes

--- 2app.js
+++ 2app.js
@@ -1,15 +1,40 @@
-const todoPage = document.querySelector('.todo-list-page');
-const header = document.querySelector('.header');
-const todoForm =document.querySelector('.todo-form');
-const todoFormInput =document.querySelector('.todo-form input[type="text"]')
-// const todoFormInput =document.querySelector('.todo-list-input [type="text"')
-//using todoFormInput doesn't return expected selection
-const submitBtn =document.querySelector('.todo-form input[type="submit"]');
-const todoList= document.querySelector ('.todo-list');
-const todoListItemPlaceholder = document.querySelector('.todo-list-item-placeholder');
+//QUERIES
 
-//SET UP EVENT LISTENER FOR FORM AND PREVENT DEFAULT CLEARING OF FORM
+const todoForm =document.querySelector('.todo-form')
+const todoFormInput=document.querySelector('input#task.todo-list-input')
+const todoList=document.querySelector('ul.todo-list')
 
-todoForm.addEventListener('submit', function(e){
+// //SET UP EVENT LISTENER FOR FORM AND PREVENT DEFAULT CLEARING OF FORM
+
+todoForm.addEventListener('submit',function(e){
     e.preventDefault();
+    addNewTodo();
+    addRemoveBtn();
+
+
+//  const newTodoItem =document.createElement('li');
+//  newTodoItem.innerText = todoFormInput.value;
+//  todoListItem.append(newTodoItem)
+
 })
+
+
+// CREATE A NEW LI FOR THE TODOLIST (for now we will put this inside our todoForm function)
+//SETTING the text of the newTodoItem. If we type todoFormInput.value into the console, we should see whatever we 
+//submitted in the form:
+//Now we want to append the new LI to our todoList
+
+//FUNCTIONS
+
+function addNewTodo(){
+    const newTodoItem =document.createElement('li');
+    newTodoItem.innerText = todoFormInput.value;     
+    todoList.append(newTodoItem);
+}
+
+function addRemoveBtn (){
+    const removeBtn=document.createElement('button');     
+    removeBtn.innerText="X";     
+    todoList.append(removeBtn);
+   
+}
