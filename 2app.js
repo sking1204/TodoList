b2210afc0de77e9593e56924eeb38cff6a8c8b72
Shanaz King <sking1128@YewYew1128.localdomain>
2023-06-02 14:20:20 -0400

    updated html jss cs

---
+++ 2app.js
@@ -0,0 +1,6 @@
+const todoPage = document.querySelector('.todo-list-page');
+const header = document.querySelector('.header');
+const todoListInput =document.querySelector('.todo-form');
+const submitbtn =document.querySelector('.todo-form input[type="submit"]');
+const todoList= document.querySelector ('.todo-list');
+const todoListItemPlaceholder = document.querySelector('.todo-list-item-placeholder');
