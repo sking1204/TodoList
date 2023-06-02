a59217374dd5cc984594b467e6a954f1dbe0381d
Shanaz King <sking1128@YewYew1128.localdomain>
2023-06-02 14:21:26 -0400

    added 2appjs

--- 2app.js
+++ 2app.js
@@ -1,6 +1,7 @@
 const todoPage = document.querySelector('.todo-list-page');
-const header = document.querySelector('.header');
+const header = document.querySelector('..header');
 const todoListInput =document.querySelector('.todo-form');
-const submitbtn =document.querySelector('.todo-form input[type="submit"]');
+const submitBtn =document.querySelector('.todo-form input[type="submit"]');
 const todoList= document.querySelector ('.todo-list');
 const todoListItemPlaceholder = document.querySelector('.todo-list-item-placeholder');
+
